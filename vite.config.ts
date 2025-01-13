5a0cd4b90be44252f23c4c71922ade47245f6f77
Christopher Ajayi <christopher@codingtemple.com>
2025-01-13 05:05:28 -0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [visualizer(), react()],
   build: {
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output:{
         manualChunks: {
